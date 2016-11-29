d78e30146243c923bcc1744947ab06a29dbd84ef
kevin-jukedeck <kevin@jukedeck.com>
2016-11-29 21:42:10 +0000

    Add vae and functionaltools modules

---
+++ nn_models/vae.py
@@ -0,0 +1,291 @@
+from datetime import datetime
+import os
+import re
+import sys
+
+import numpy as np
+import tensorflow as tf
+
+from layers import Dense
+import plot
+from utils import composeAll, print_
+
+
+class VAE():
+    """Variational Autoencoder
+
+    see: Kingma & Welling - Auto-Encoding Variational Bayes
+    (http://arxiv.org/abs/1312.6114)
+    """
+    DEFAULTS = {
+        "batch_size": 128,
+        "learning_rate": 1E-3,
+        "dropout": 1.,
+        "lambda_l2_reg": 0.,
+        "nonlinearity": tf.nn.elu,
+        "squashing": tf.nn.sigmoid
+    }
+    RESTORE_KEY = "to_restore"
+
+    def __init__(self, architecture=[], d_hyperparams={}, meta_graph=None,
+                 save_graph_def=True, log_dir="./log"):
+        """(Re)build a symmetric VAE model with given:
+
+            * architecture (list of nodes per encoder layer); e.g.
+               [1000, 500, 250, 10] specifies a VAE with 1000-D inputs, 10-D latents,
+               & end-to-end architecture [1000, 500, 250, 10, 250, 500, 1000]
+
+            * hyperparameters (optional dictionary of updates to `DEFAULTS`)
+        """
+        self.architecture = architecture
+        self.__dict__.update(VAE.DEFAULTS, **d_hyperparams)
+        self.sesh = tf.Session()
+
+        if not meta_graph: # new model
+            self.datetime = datetime.now().strftime(r"%y%m%d_%H%M")
+            assert len(self.architecture) > 2, \
+                "Architecture must have more layers! (input, 1+ hidden, latent)"
+
+            # build graph
+            handles = self._buildGraph()
+            for handle in handles:
+                tf.add_to_collection(VAE.RESTORE_KEY, handle)
+            self.sesh.run(tf.initialize_all_variables())
+
+        else: # restore saved model
+            model_datetime, model_name = os.path.basename(meta_graph).split("_vae_")
+            self.datetime = "{}_reloaded".format(model_datetime)
+            *model_architecture, _ = re.split("_|-", model_name)
+            self.architecture = [int(n) for n in model_architecture]
+
+            # rebuild graph
+            meta_graph = os.path.abspath(meta_graph)
+            tf.train.import_meta_graph(meta_graph + ".meta").restore(
+                self.sesh, meta_graph)
+            handles = self.sesh.graph.get_collection(VAE.RESTORE_KEY)
+
+        # unpack handles for tensor ops to feed or fetch
+        (self.x_in, self.dropout_, self.z_mean, self.z_log_sigma,
+         self.x_reconstructed, self.z_, self.x_reconstructed_,
+         self.cost, self.global_step, self.train_op) = handles
+
+        if save_graph_def: # tensorboard
+            self.logger = tf.train.SummaryWriter(log_dir, self.sesh.graph)
+
+    @property
+    def step(self):
+        """Train step"""
+        return self.global_step.eval(session=self.sesh)
+
+    def _buildGraph(self):
+        x_in = tf.placeholder(tf.float32, shape=[None, # enables variable batch size
+                                                 self.architecture[0]], name="x")
+        dropout = tf.placeholder_with_default(1., shape=[], name="dropout")
+
+        # encoding / "recognition": q(z|x)
+        encoding = [Dense("encoding", hidden_size, dropout, self.nonlinearity)
+                    # hidden layers reversed for function composition: outer -> inner
+                    for hidden_size in reversed(self.architecture[1:-1])]
+        h_encoded = composeAll(encoding)(x_in)
+
+        # latent distribution parameterized by hidden encoding
+        # z ~ N(z_mean, np.exp(z_log_sigma)**2)
+        z_mean = Dense("z_mean", self.architecture[-1], dropout)(h_encoded)
+        z_log_sigma = Dense("z_log_sigma", self.architecture[-1], dropout)(h_encoded)
+
+        # kingma & welling: only 1 draw necessary as long as minibatch large enough (>100)
+        z = self.sampleGaussian(z_mean, z_log_sigma)
+
+        # decoding / "generative": p(x|z)
+        decoding = [Dense("decoding", hidden_size, dropout, self.nonlinearity)
+                    for hidden_size in self.architecture[1:-1]] # assumes symmetry
+        # final reconstruction: restore original dims, squash outputs [0, 1]
+        decoding.insert(0, Dense( # prepend as outermost function
+            "x_decoding", self.architecture[0], dropout, self.squashing))
+        x_reconstructed = tf.identity(composeAll(decoding)(z), name="x_reconstructed")
+
+        # reconstruction loss: mismatch b/w x & x_reconstructed
+        # binary cross-entropy -- assumes x & p(x|z) are iid Bernoullis
+        rec_loss = VAE.crossEntropy(x_reconstructed, x_in)
+
+        # Kullback-Leibler divergence: mismatch b/w approximate vs. imposed/true posterior
+        kl_loss = VAE.kullbackLeibler(z_mean, z_log_sigma)
+
+        with tf.name_scope("l2_regularization"):
+            regularizers = [tf.nn.l2_loss(var) for var in self.sesh.graph.get_collection(
+                "trainable_variables") if "weights" in var.name]
+            l2_reg = self.lambda_l2_reg * tf.add_n(regularizers)
+
+        with tf.name_scope("cost"):
+            # average over minibatch
+            cost = tf.reduce_mean(rec_loss + kl_loss, name="vae_cost")
+            cost += l2_reg
+
+        # optimization
+        global_step = tf.Variable(0, trainable=False)
+        with tf.name_scope("Adam_optimizer"):
+            optimizer = tf.train.AdamOptimizer(self.learning_rate)
+            tvars = tf.trainable_variables()
+            grads_and_vars = optimizer.compute_gradients(cost, tvars)
+            clipped = [(tf.clip_by_value(grad, -5, 5), tvar) # gradient clipping
+                    for grad, tvar in grads_and_vars]
+            train_op = optimizer.apply_gradients(clipped, global_step=global_step,
+                                                 name="minimize_cost")
+
+        # ops to directly explore latent space
+        # defaults to prior z ~ N(0, I)
+        with tf.name_scope("latent_in"):
+            z_ = tf.placeholder_with_default(tf.random_normal([1, self.architecture[-1]]),
+                                            shape=[None, self.architecture[-1]],
+                                            name="latent_in")
+        x_reconstructed_ = composeAll(decoding)(z_)
+
+        return (x_in, dropout, z_mean, z_log_sigma, x_reconstructed,
+                z_, x_reconstructed_, cost, global_step, train_op)
+
+    def sampleGaussian(self, mu, log_sigma):
+        """(Differentiably!) draw sample from Gaussian with given shape, subject to random noise epsilon"""
+        with tf.name_scope("sample_gaussian"):
+            # reparameterization trick
+            epsilon = tf.random_normal(tf.shape(log_sigma), name="epsilon")
+            return mu + epsilon * tf.exp(log_sigma) # N(mu, I * sigma**2)
+
+    @staticmethod
+    def crossEntropy(obs, actual, offset=1e-7):
+        """Binary cross-entropy, per training example"""
+        # (tf.Tensor, tf.Tensor, float) -> tf.Tensor
+        with tf.name_scope("cross_entropy"):
+            # bound by clipping to avoid nan
+            obs_ = tf.clip_by_value(obs, offset, 1 - offset)
+            return -tf.reduce_sum(actual * tf.log(obs_) +
+                                  (1 - actual) * tf.log(1 - obs_), 1)
+
+    @staticmethod
+    def l1_loss(obs, actual):
+        """L1 loss (a.k.a. LAD), per training example"""
+        # (tf.Tensor, tf.Tensor, float) -> tf.Tensor
+        with tf.name_scope("l1_loss"):
+            return tf.reduce_sum(tf.abs(obs - actual) , 1)
+
+    @staticmethod
+    def l2_loss(obs, actual):
+        """L2 loss (a.k.a. Euclidean / LSE), per training example"""
+        # (tf.Tensor, tf.Tensor, float) -> tf.Tensor
+        with tf.name_scope("l2_loss"):
+            return tf.reduce_sum(tf.square(obs - actual), 1)
+
+    @staticmethod
+    def kullbackLeibler(mu, log_sigma):
+        """(Gaussian) Kullback-Leibler divergence KL(q||p), per training example"""
+        # (tf.Tensor, tf.Tensor) -> tf.Tensor
+        with tf.name_scope("KL_divergence"):
+            # = -0.5 * (1 + log(sigma**2) - mu**2 - sigma**2)
+            return -0.5 * tf.reduce_sum(1 + 2 * log_sigma - mu**2 -
+                                        tf.exp(2 * log_sigma), 1)
+
+    def encode(self, x):
+        """Probabilistic encoder from inputs to latent distribution parameters;
+        a.k.a. inference network q(z|x)
+        """
+        # np.array -> [float, float]
+        feed_dict = {self.x_in: x}
+        return self.sesh.run([self.z_mean, self.z_log_sigma], feed_dict=feed_dict)
+
+    def decode(self, zs=None):
+        """Generative decoder from latent space to reconstructions of input space;
+        a.k.a. generative network p(x|z)
+        """
+        # (np.array | tf.Variable) -> np.array
+        feed_dict = dict()
+        if zs is not None:
+            is_tensor = lambda x: hasattr(x, "eval")
+            zs = (self.sesh.run(zs) if is_tensor(zs) else zs) # coerce to np.array
+            feed_dict.update({self.z_: zs})
+        # else, zs defaults to draw from conjugate prior z ~ N(0, I)
+        return self.sesh.run(self.x_reconstructed_, feed_dict=feed_dict)
+
+    def vae(self, x):
+        """End-to-end autoencoder"""
+        # np.array -> np.array
+        return self.decode(self.sampleGaussian(*self.encode(x)))
+
+    def train(self, X, max_iter=np.inf, max_epochs=np.inf, cross_validate=True,
+              verbose=True, save=True, outdir="./out", plots_outdir="./png",
+              plot_latent_over_time=False):
+        if save:
+            saver = tf.train.Saver(tf.all_variables())
+
+        try:
+            err_train = 0
+            now = datetime.now().isoformat()[11:]
+            print("------- Training begin: {} -------\n".format(now))
+
+            if plot_latent_over_time: # plot latent space over log_BASE time
+                BASE = 2
+                INCREMENT = 0.5
+                pow_ = 0
+
+            while True:
+                x, _ = X.train.next_batch(self.batch_size)
+                feed_dict = {self.x_in: x, self.dropout_: self.dropout}
+                fetches = [self.x_reconstructed, self.cost, self.global_step, self.train_op]
+                x_reconstructed, cost, i, _ = self.sesh.run(fetches, feed_dict)
+
+                err_train += cost
+
+                if plot_latent_over_time:
+                    while int(round(BASE**pow_)) == i:
+                        plot.exploreLatent(self, nx=30, ny=30, ppf=True, outdir=plots_outdir,
+                                           name="explore_ppf30_{}".format(pow_))
+
+                        names = ("train", "validation", "test")
+                        datasets = (X.train, X.validation, X.test)
+                        for name, dataset in zip(names, datasets):
+                            plot.plotInLatent(self, dataset.images, dataset.labels, range_=
+                                              (-6, 6), title=name, outdir=plots_outdir,
+                                              name="{}_{}".format(name, pow_))
+
+                        print("{}^{} = {}".format(BASE, pow_, i))
+                        pow_ += INCREMENT
+
+                if i%1000 == 0 and verbose:
+                    print("round {} --> avg cost: ".format(i), err_train / i)
+
+                if i%2000 == 0 and verbose:# and i >= 10000:
+                    # visualize `n` examples of current minibatch inputs + reconstructions
+                    plot.plotSubset(self, x, x_reconstructed, n=10, name="train",
+                                    outdir=plots_outdir)
+
+                    if cross_validate:
+                        x, _ = X.validation.next_batch(self.batch_size)
+                        feed_dict = {self.x_in: x}
+                        fetches = [self.x_reconstructed, self.cost]
+                        x_reconstructed, cost = self.sesh.run(fetches, feed_dict)
+
+                        print("round {} --> CV cost: ".format(i), cost)
+                        plot.plotSubset(self, x, x_reconstructed, n=10, name="cv",
+                                        outdir=plots_outdir)
+
+                if i >= max_iter or X.train.epochs_completed >= max_epochs:
+                    print("final avg cost (@ step {} = epoch {}): {}".format(
+                        i, X.train.epochs_completed, err_train / i))
+                    now = datetime.now().isoformat()[11:]
+                    print("------- Training end: {} -------\n".format(now))
+
+                    if save:
+                        outfile = os.path.join(os.path.abspath(outdir), "{}_vae_{}".format(
+                            self.datetime, "_".join(map(str, self.architecture))))
+                        saver.save(self.sesh, outfile, global_step=self.step)
+                    try:
+                        self.logger.flush()
+                        self.logger.close()
+                    except(AttributeError): # not logging
+                        continue
+                    break
+
+        except(KeyboardInterrupt):
+            print("final avg cost (@ step {} = epoch {}): {}".format(
+                i, X.train.epochs_completed, err_train / i))
+            now = datetime.now().isoformat()[11:]
+            print("------- Training end: {} -------\n".format(now))
+            sys.exit(0)
---
+++ utils/functionaltools.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Samer
+"""
+from functools import partial, reduce
+
+class Fail(Exception):
+    pass
+
+def head(l):           return l[0]
+def tail(l):           return l[1:]
+
+# def fst((x, _)):  return x
+# def snd((_, x)):  return x
+# def swap((x, y)): return (y, x)
+def identity(x):  return x
+def const(x):     return lambda _: x
+def flip(f):      return lambda x, y: f(y, x)
+def app(f, x): return f(x)
+def apply_to(x):  return lambda f: f(x)
+def pair_with(x): return lambda y: (x, y)
+def divby(k):     return lambda x: x/k
+
+def guard(f, x):
+    if f(x):
+        return x
+    raise Fail
+
+def exclude_none(x, msg=""):
+    """ :: maybe(A), msg:str='' -> A
+    Throw exception with msg if x is None, otherwise return x
+    """
+    if x is None:
+        raise LookupError(msg)
+    return x
+
+def if_none_else(fallback, value):
+    """ :: A, maybe(A) -> A """
+    return fallback if value is None else value
+
+def member(X):    return lambda x: x in X
+def not_member(X): return lambda x: x not in X
+
+def compose2(f, g): return lambda x: f(g(x))
+def compose(*args): return reduce(compose2, args)
+
+# def ffst(f): return lambda (x, y): (f(x), y)
+# def fsnd(f): return lambda (x, y): (x, f(y))
+
+def bind(f, *args, **kwargs): return partial(f, *args, **kwargs)
+
+def split(n, l):
+    return l[:n], l[n:]
+
+def foldl(f, xs, s): return reduce(flip(f), xs, s)
+def foldr(f, xs, s): return reduce(flip(f), reversed(xs), s)
+
+def map_accum(f, xs, s):
+    """ :: (a,s->(b,s)), list(a), s -> (list(b), s) """
+    ys=[]
+    for x in xs:
+        (y, s) = f(x, s)
+        ys.append(y)
+    return (ys, s)
+
+def map_filter(f, items):
+    return [x[0] for x in map(f, items) if x != []]
+
+def unsingleton(l):
+    if len(l) != 1:
+        raise ValueError(("List is not a singleton", l))
+    return head(l)
+
+def diff_with(f, xs):
+    return [f(xs[i], xs[i-1]) for i in range(1, len(xs))]
+
+def scanr(f, xs, s):
+    ss=[]
+    for x in reversed(xs):
+        s = f(x, s)
+        ss.insert(0, s)
+    return ss
+
+def partition(cond, items):
+    """ :: (a->bool), list(a) -> (list(a), list(a)) """
+    (sheep, goats) = ([], [])
+    for i in items: (sheep if cond(i) else goats).append(i)
+    return (goats, sheep)
+
+def maybe(f, x):
+    """ :: (A->B), maybe(A) -> maybe(B)
+    Apply function if not None, otherwise, just keep the None."""
+    if x is None:
+        return None
+    else:
+        return f(x)
+
+def table(domain, f):
+    return {x: f(x) for x in domain}.__getitem__
+
+def memoise(f):
+    memo = {}
+    def g(x):
+        y = memo.get(x, None)
+        if y is None:
+            y = f(x)
+            memo[x] = y
+        return y
+    return g
+
+def lazy(f):
+    result = [None]
+    def g():
+        if result[0] is None:
+            result[0] = f()
+        return result[0]
+    return g
+
+def unzip(tuples):
+    """
+    :: list((a,b,...)) -> list(a), list(b), ... | ()
+    Unzip list of tuples into a tuple of lists. Works for any arity.
+    NB: if tuples list is empty, returns empty tuple, NOT tuple of empty lists.
+    Use unzip2 for strict unzipping of list of pairs.
+    """
+    return tuple(map(list, zip(*tuples)))
+
+def unzip2(pairs):
+    """ :: list((a,b)) -> list(a), list(b) """
+    if len(pairs) == 0: return [], []
+    x, y = zip(*pairs)
+    return list(x), list(y)
+
+def for_each(f, it):
+    for x in it: f(x)
+
+# predicates
+
+def not_none(x): return x is not None
+def nonneg(x):   return x>=0
+def positive(x): return x>0
+def natural(x):  return x>=0 and int(x) == x
+def not_in(y):   return lambda x: x not in y
+def leq(y): return lambda x: x<=y
+def geq(y): return lambda x: x>=y
+def gt(y):  return lambda x: x>y
+def lt(y):  return lambda x: x<y
+def neq(y): return lambda x: x!=y
+def eq(y):  return lambda x: x==y
+
+def repeat_until_success_or_exhaustion(n_tries, criterion, description, action,
+                                       initial_state=None,
+                                       must_succeed=False):
+    """ :: natural, (A->bool), string, (->A), A, bool -> A """
+    ret = initial_state
+    for _ in range(n_tries):
+        ret = action()
+        if criterion(ret):
+            return ret
+    if must_succeed:
+        raise RuntimeError("Failed to %r after %r attempts" % (description, n_tries))
+    else:
+        return ret
