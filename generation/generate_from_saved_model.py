Add vae, fix strange scaling, and add some debugging tools

generation/generate_from_saved_model.py
@@ -4,13 +4,17 @@ import json
 import os
 from models.utilFunctions import nextbiggestpower2, window_dictionary
 from models.sineModel import sineModelSynth
-from models.stft import stftSynth
+from models.stft import stftSynth, stftAnal
 import soundfile
 from nn_models.rnn_models import SimpleLSTM
 from datetime import datetime
 from utils.utils import load_saved_model_to_resume_training
 from utils.generation_utils import SineModelOutputProcessing, SineModelOutputProcessingWithActiveTracking
 from utils.generation_utils import STFTModelOutputProcessing
+from utils.vectorisation_utils import load_from_dir_root
+from training.setup.setup_data import setup_training_data
+
+from plotting import spectogram_plot
 
 STARTED_DATESTRING = "{0:%Y-%m-%dT%H-%M-%S}".format(datetime.now())
 
@@ -92,8 +96,55 @@ def main(args):
         final_outputs = tf.pack(final_outputs)  # Make one tensor of rank 2
         print(final_outputs.get_shape())
         final_outputs = tf.transpose(final_outputs,
-                                     [1, 0, 2])  # final_outputs has shape (batch_size, n_frames, n_hidden)
+                                     [1, 0, 2])  # final_outputs has shape (batch_size, n_frames, n_outputs)
         print('after packing and transposing, final_outputs shape: ', final_outputs.get_shape())
+
+        ####################
+        # Compare to ground truth (debugging)
+        if args.vector_folder is None:
+            print("No vector folder was provided, cannot calculate cost for debugging.")
+        else:
+            loaded, json_vector_settings, analysis_type = load_from_dir_root(args.vector_folder)
+            _, data_dict = setup_training_data(loaded, 1)  # batch_size = 1
+            ground_truth = data_dict['output_data']  # (n_frames, batch_size, n_outputs)
+            ground_truth = np.transpose(ground_truth, [1, 0, 2])
+            print('Network generation: {}'.format(final_outputs.eval()))
+            print('Data: {}'.format(ground_truth))
+            print(ground_truth.shape)
+            print(final_outputs.eval().shape)
+            assert ground_truth.shape == final_outputs.eval().shape
+            print('Squared error achieved by network: {}'.format(np.sum((ground_truth - final_outputs.eval())**2)
+                                                                 / ground_truth.size))
+            # mX = final_outputs.eval()[0,:,:257]
+            # pX = final_outputs.eval()[0,:,257:]
+            #
+            # np.save('./mX_model', mX)
+            # np.save('./pX_model', pX)
+            #
+            # asdfasdf
+            # phase_range = network_settings['stft_settings']['phase_range']
+            # mag_range = network_settings['stft_settings']['mag_range']
+            #
+            # # Unnormalise
+            # mX = mag_range[0] + (mX * (mag_range[1] - mag_range[0]))
+            # mX *= sr / 2  # Undo weird rescaling
+            # pX = phase_range[0] + (pX * (phase_range[1] - phase_range[0]))
+            #
+            # print(mX.shape)  # (num_frames, num_freq_bins)
+            # # mX = np.transpose(mX)
+            # # pX = np.transpose(pX)
+            # spectogram_plot(mX, pX, M, N, H, sr, fig_number=None, filepath=None, show=True)
+            #
+            # reconst = stftSynth(mX, pX, M, H)
+            # print(np.max(reconst), np.min(reconst))
+            #
+            #
+            # # soundfile.write('./test_reconst.wav', reconst, sr, format='wav')
+            # mX2, pX2 = stftAnal(reconst, w, N, H)
+            # print(mX2.shape, pX2.shape)
+            # spectogram_plot(mX2, pX2, M, N, H, sr, fig_number=None, filepath=None, show=True)
+            # asdfasdfasd
+
 
         ####################
         # For testing - just batch size = 1
@@ -110,7 +161,7 @@ def main(args):
     elif analysis_type == 'stft':
         process_output = STFTModelOutputProcessing(result, network_settings)
         xtmag, xtphase = process_output.convert_network_output_to_analysis_model_input()
-        reconstruction = stftSynth(xtmag, xtphase, analysis_settings['M'], H)
+        reconstruction = stftSynth(xtmag, xtphase, M, H)
     else:
         raise Exception('analysis_type not recognised!')
 
@@ -121,7 +172,7 @@ def main(args):
                filepath='./generation/plots/{}-(generated_{})/'.format(model_name, STARTED_DATESTRING))
 
     soundfile.write('./generation/wav_output/{}-(generated_{}).wav'.format(model_name, STARTED_DATESTRING),
-                    reconstruction, analysis_settings['sample_rate'], format='wav')
+                    reconstruction, sr, format='wav')
     #
     # np.save('./xtfreq', xtfreq)
     # np.save('./xtmag', xtmag)

nn_models/rnn_models.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class SimpleLSTM(object):
 
-    def __init__(self, input_placeholder, state_placeholder, n_hidden, n_outputs):
+    def __init__(self, input_placeholder, state_placeholder, n_hidden, n_outputs, activation_fn=tf.sigmoid):
         """
         Sets up the LSTM model with an additional output filter to shape to size n_outputs
         :param input_placeholder: Placeholder tensor of shape (n_steps, batch_size, n_inputs)
@@ -21,6 +21,7 @@ class SimpleLSTM(object):
         self.weights_out = []
         self.biases_out = []
         self.cell = []
+        self.activation_fn = activation_fn
         print('n_outputs:', n_outputs)
         print('n_hidden:', n_hidden)
 
@@ -82,7 +83,7 @@ class SimpleLSTM(object):
                 final_states.append(states)
 
                 # Linear activation, using rnn inner loop
-                final_output = [tf.sigmoid(tf.matmul(output, self.weights_out[i]) + self.biases_out[i]) for output in outputs]
+                final_output = [self.activation_fn(tf.matmul(output, self.weights_out[i]) + self.biases_out[i]) for output in outputs]
                 print('each final_outputs shape: ', final_output[0].get_shape())
                 print('length of final_outputs: ', len(final_output))
                 x = final_output

nn_models/vae.py
@@ -6,9 +6,9 @@ import sys
 import numpy as np
 import tensorflow as tf
 
-from layers import Dense
-import plot
-from utils import composeAll, print_
+import utils.vaeplot as vaeplot
+from utils.functionaltools import composeAll
+from utils.utils import print_
 
 
 class VAE():
@@ -27,7 +27,7 @@ class VAE():
     }
     RESTORE_KEY = "to_restore"
 
-    def __init__(self, architecture=[], d_hyperparams={}, meta_graph=None,
+    def __init__(self, architecture=[], d_hyperparams={},
                  save_graph_def=True, log_dir="./log"):
         """(Re)build a symmetric VAE model with given:
 
@@ -41,28 +41,15 @@ class VAE():
         self.__dict__.update(VAE.DEFAULTS, **d_hyperparams)
         self.sesh = tf.Session()
 
-        if not meta_graph: # new model
-            self.datetime = datetime.now().strftime(r"%y%m%d_%H%M")
-            assert len(self.architecture) > 2, \
-                "Architecture must have more layers! (input, 1+ hidden, latent)"
+        self.datetime = datetime.now().strftime(r"%y%m%d_%H%M")
+        assert len(self.architecture) > 2, \
+            "Architecture must have more layers! (input, 1+ hidden, latent)"
 
-            # build graph
-            handles = self._buildGraph()
-            for handle in handles:
-                tf.add_to_collection(VAE.RESTORE_KEY, handle)
-            self.sesh.run(tf.initialize_all_variables())
-
-        else: # restore saved model
-            model_datetime, model_name = os.path.basename(meta_graph).split("_vae_")
-            self.datetime = "{}_reloaded".format(model_datetime)
-            *model_architecture, _ = re.split("_|-", model_name)
-            self.architecture = [int(n) for n in model_architecture]
-
-            # rebuild graph
-            meta_graph = os.path.abspath(meta_graph)
-            tf.train.import_meta_graph(meta_graph + ".meta").restore(
-                self.sesh, meta_graph)
-            handles = self.sesh.graph.get_collection(VAE.RESTORE_KEY)
+        # build graph
+        handles = self._buildGraph()
+        for handle in handles:
+            tf.add_to_collection(VAE.RESTORE_KEY, handle)
+        self.sesh.run(tf.initialize_all_variables())
 
         # unpack handles for tensor ops to feed or fetch
         (self.x_in, self.dropout_, self.z_mean, self.z_log_sigma,
@@ -235,13 +222,13 @@ class VAE():
 
                 if plot_latent_over_time:
                     while int(round(BASE**pow_)) == i:
-                        plot.exploreLatent(self, nx=30, ny=30, ppf=True, outdir=plots_outdir,
+                        vaeplot.exploreLatent(self, nx=30, ny=30, ppf=True, outdir=plots_outdir,
                                            name="explore_ppf30_{}".format(pow_))
 
                         names = ("train", "validation", "test")
                         datasets = (X.train, X.validation, X.test)
                         for name, dataset in zip(names, datasets):
-                            plot.plotInLatent(self, dataset.images, dataset.labels, range_=
+                            vaeplot.plotInLatent(self, dataset.images, dataset.labels, range_=
                                               (-6, 6), title=name, outdir=plots_outdir,
                                               name="{}_{}".format(name, pow_))
 
@@ -253,7 +240,7 @@ class VAE():
 
                 if i%2000 == 0 and verbose:# and i >= 10000:
                     # visualize `n` examples of current minibatch inputs + reconstructions
-                    plot.plotSubset(self, x, x_reconstructed, n=10, name="train",
+                    vaeplot.plotSubset(self, x, x_reconstructed, n=10, name="train",
                                     outdir=plots_outdir)
 
                     if cross_validate:
@@ -263,7 +250,7 @@ class VAE():
                         x_reconstructed, cost = self.sesh.run(fetches, feed_dict)
 
                         print("round {} --> CV cost: ".format(i), cost)
-                        plot.plotSubset(self, x, x_reconstructed, n=10, name="cv",
+                        vaeplot.plotSubset(self, x, x_reconstructed, n=10, name="cv",
                                         outdir=plots_outdir)
 
                 if i >= max_iter or X.train.epochs_completed >= max_epochs: