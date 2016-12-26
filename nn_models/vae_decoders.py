871ea2e7e1e032e767f0b5962ce6c4c4150a7aa0
kevin-jukedeck <kevin@jukedeck.com>
2016-12-26 20:52:43 +0100

    Add initialisers to decoder and fix feedback

--- nn_models/vae_decoders.py
+++ nn_models/vae_decoders.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 from nn_models.layers import FeedForward, Dense
 from nn_models.rnn_models import SimpleLSTM
+from nn_models.initialisers import wbVars_Xavier
+from tensorflow.python.ops import variable_scope as vs
 
 
 class FeedForwardDecoder(object):
@@ -52,30 +54,37 @@ class LSTMDecoder(object):
         # encoding / recognition model q(z|x)
         batch_size = z.get_shape()[0]
 
-        lstm_decoder = SimpleLSTM(self.n_hidden, scope='LSTM_decoder')
+        # with tf.variable_scope('LSTM_decoder') as decoder_scope:
+        lstm_decoder = SimpleLSTM(self.n_hidden, scope="LSTM_decoder")
         lstm_activation = lstm_decoder.lstm_activation
 
         # (Cell state, hidden state):
-        init_states = (Dense(scope="latent_to_LSTM_cell", size=self.n_hidden, nonlinearity=lstm_activation)(z),
-                       Dense(scope="latent_to_LSTM_hidden", size=self.n_hidden, nonlinearity=lstm_activation)(z))
+        init_states = (Dense(scope="latent_to_LSTM_cell", size=self.n_hidden, nonlinearity=lstm_activation,
+                             initialiser=wbVars_Xavier)(z),
+                       Dense(scope="latent_to_LSTM_hidden", size=self.n_hidden, nonlinearity=lstm_activation,
+                             initialiser=wbVars_Xavier)(z))
 
         # input = tf.placeholder(tf.float32, shape=[1, batch_size, self.n_outputs])  # (n_steps, batch_size, n_inputs)
         # states = (tf.placeholder(tf.float32, shape=[batch_size, self.n_hidden]),
         #           tf.placeholder(tf.float32, shape=[batch_size, self.n_hidden]))
 
         first_input = tf.zeros((1, batch_size, self.n_outputs))  # NB Just one step, so first argument is 1
-
-        dense_output = Dense(scope="LSTM_dense_output", size=self.n_outputs, nonlinearity=self.output_activation)
+        dense_output = Dense(scope="LSTM_decoder/dense_output", size=self.n_outputs, nonlinearity=self.output_activation,
+                             initialiser=wbVars_Xavier)
 
         # feed_dict = {input: first_input, states[0]: first_states[0], states[1]: first_states[1]}
         lstm_input = first_input
         states = init_states
         final_outputs = []
         for step in range(self.n_steps):
-            outputs, final_state = lstm_decoder(lstm_input, states)  # outputs shape is (n_steps, batch_size, n_hidden)
+            # print("STEP: ", step)
+            outputs, final_state = lstm_decoder(lstm_input, states)
             outputs_list = tf.unpack(outputs)  # List of length 1, element shape (batch_size, n_hidden)
-            final_outputs.append(dense_output(outputs_list[0]))
-            lstm_input = outputs
+            final_output = dense_output(outputs_list[0])  # (batch_size, n_outputs)
+            final_outputs.append(final_output)
+            lstm_input = tf.pack([final_output])  # (1, batch_size, n_outputs)
             states = final_state
 
-        return final_outputs, states
+        final_outputs = tf.pack(final_outputs)  # (n_steps, batch_size, n_outputs)
+        print('final outputs shape from decoder: ', final_outputs.get_shape())
+        return final_outputs
