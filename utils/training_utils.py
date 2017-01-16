e49b98382f901370456bf7680b98f52b97ecb78d
kevin-jukedeck <kevin@jukedeck.com>
2017-01-16 15:46:40 +0000

    Create gradient accumulator tools

--- utils/training_utils.py
+++ utils/training_utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import tensorflow as tf
 
 class Patience(object):
     """
@@ -58,4 +58,63 @@ class PlateauDetector(object):
             return True
         else:
             return False
+
+
+class GradientAccumulator(object):
+    """
+    Accumulates the gradients for trainable variables to allow training steps to be broken into several iterations,
+    when the minibatch is too big to fit on the GPU.
+    """
+
+    def __init__(self, grads_and_vars, optimizer):
+        """
+        Initialiser
+        :param gradients: List of (gradient, variable) tuples as returned by the optimizer.compute_gradients op
+        """
+        self._gradients_and_variables = grads_and_vars
+        self._optimizer = optimizer
+
+        # The gradients returned from compute_gradients is a list of (gradient tensor, Variable object) tuples
+        self._var_to_grad = dict((var.name, grad) for grad, var in self._gradients_and_variables)
+        self._varname_to_var = dict((var.name, var) for _, var in self._gradients_and_variables)
+        self._var_keys = sorted(self._varname_to_var.keys())
+        self._var_to_accum_grad = self._initialize_gradient_dict()
+
+    def _initialize_gradient_dict(self):
+        var_to_acc_grad = {}
+        print("Constructing the gradient accumulator dict")
+        for k in self._var_keys:
+            with tf.variable_scope('grad_acc'):
+                grad = self._var_to_grad[k]
+                print(k, grad.get_shape())
+                acc = tf.Variable(tf.zeros(grad.get_shape()), trainable=False)
+                var_to_acc_grad[k] = acc
+        return var_to_acc_grad
+
+    def clear_gradients(self):
+        """Returns a list of ops to reset all stored gradients to zero"""
+        print("Clearing the gradients in the accumulator dict")
+        clear_ops = []
+        for k in self._var_keys:
+            grad = self._var_to_grad[k]
+            clear_ops.append(self._var_to_accum_grad[k].assign(tf.zeros(grad.get_shape())))
+        return clear_ops
+
+    def update_gradients_ops(self):
+        """Returns a list of ops to update all gradients in the _var_to_accum_grad dict"""
+        print("Updating gradients")
+        update_ops = []
+        for k in self._var_keys:
+            grad = self._var_to_grad[k]
+            update_ops.append(self._var_to_accum_grad[k].assign_add(grad))
+        return update_ops
+
+    def cumulative_gradient_list(self):
+        """Returns a list of (grad, var) to be used in the optimizer.apply_gradients method"""
+        var_grad_list = []
+        for k in self._var_keys:
+            grad = self._var_to_accum_grad[k]
+            var = self._varname_to_var[k]
+            var_grad_list.append((grad, var))
+        return var_grad_list
 
